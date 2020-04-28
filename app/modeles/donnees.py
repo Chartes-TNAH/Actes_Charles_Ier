23f006376ff0226d06135e66806ea7354604505d
jeandamien-genero <jeandamien.genero@gmail.com>
2020-04-28 23:17:36 +0200

    travail sur data.py (à poursuivre

--- app/modeles/donnees.py
+++ app/modeles/donnees.py
@@ -21,32 +21,4 @@ class Acts(db.Model):
         self.date = date
         self.place = place
         self.analyse = analyse
-
-list_id = []
-list_institution = []
-list_state = []
-list_type = []
-list_date = []
-list_place = []
-list_analyse = []
-number = 0
-id = 0
-
-for item in source_doc.xpath("//group/text"):
-    number += 1
-    item_institution = source_doc.xpath("//text[@n=" + str(number) + "]//institution/text()")
-    list_institution.append(item_institution[0])
-    list_state.append(item.attrib['type'])
-    list_type.append(item.attrib['subtype'])
-    item_date = source_doc.xpath("//text[@n=" + str(number) + "]//date/text()")
-    list_date.append(item_date[0])
-    item_place = source_doc.xpath("//text[@n=" + str(number) + "]//docDate/placeName/text()")
-    list_place.append(item_place[0])
-    item_analyse = source_doc.xpath("//text[@n=" + str(number) + "]//argument[1]/p/text()")
-    list_analyse.append(item_analyse[0])
-for item in list_date:
-    db.drop_all()
-    db.create_all()
-    id +=1
-    db.session.add(Acts(id, list_institution[id - 1], list_state[id - 1], list_type[id - 1], list_date[id - 1], list_place[id - 1], list_analyse[id - 1]))
-db.session.commit()
+        
